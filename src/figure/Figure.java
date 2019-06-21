7ad0c8e51adf97f33600840db75e3bb3be44f754
ToDoRV <tr.velikov@gmail.com>
2019-06-21 12:15:37 +0300

    Add move command

--- src/figure/Figure.java
+++ src/figure/Figure.java
@@ -1,8 +1,17 @@
 package figure;
 
+import game.Game;
+import game.Player;
 import enums.Position;
 
 public abstract class Figure {
+
+    static final int ERROR_CODE_SUCCESS = Game.ERROR_CODE_SUCCESS;
+    static final int ERROR_CODE_TRY_AGAIN = Game.ERROR_CODE_TRY_AGAIN;
+
+    private static final String RESET_COLOR = "\u001B[0m";
+    private static final String RED = "\u001B[31m";
+    static final String INVALID_MOVE_MESSAGE = RED + "Invalid move! "  + RESET_COLOR;
 
     Position position;
     String color;
@@ -16,5 +25,10 @@ public abstract class Figure {
         return color;
     }
 
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
     public abstract void printFigure();
+    public abstract int canMove(Position toPosition, Player player, Figure[][] board);
 }
