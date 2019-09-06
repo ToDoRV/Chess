b23822c67c046f17e67ed8c5d6d7fdedad83b789
ToDoRV <tr.velikov@gmail.com>
2019-09-06 13:29:51 +0300

    Realized canMove method

--- src/figure/Knight.java
+++ src/figure/Knight.java
@@ -5,9 +5,14 @@ import enums.ErrorCode;
 import enums.Position;
 import game.Player;
 
+import static java.lang.Math.abs;
+
 public class Knight extends Figure {
 
     public static final String KNIGHT_SIGN = " N ";
+
+    public static final String TRY_TO_GET_OWN_FIGURE_MESSAGE =
+            Color.Red.getColor() + "You try to get your own figure!" + Color.ResetColor.getColor();
 
     public Knight(Position position, Color color) {
         super(position, color);
@@ -20,7 +25,18 @@ public class Knight extends Figure {
 
     @Override
     public ErrorCode canMove(Position toPosition, Player player, Figure[][] board) {
-        //ToDo
-        return ErrorCode.Success;
+        if (isThereOwnFigureOn(toPosition, player, board)) {
+            System.out.println(TRY_TO_GET_OWN_FIGURE_MESSAGE);
+            return ErrorCode.TryAgain;
+        }
+
+        int differenceInRows = abs(toPosition.getRow() - position.getRow());
+        int differenceInColumns = abs(toPosition.getColumn() - position.getColumn());
+        if (differenceInRows == 2 && differenceInColumns == 1
+                || differenceInRows == 1 && differenceInColumns == 2) {
+            return ErrorCode.Success;
+        }
+
+        return ErrorCode.TryAgain;
     }
 }
