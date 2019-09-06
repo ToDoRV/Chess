01e71832638e7bf296e9f4bc283678f8a6443411
ToDoRV <tr.velikov@gmail.com>
2019-09-06 13:32:10 +0300

    Realized canMove method

--- src/figure/Bishop.java
+++ src/figure/Bishop.java
@@ -20,7 +20,67 @@ public class Bishop extends Figure {
 
     @Override
     public ErrorCode canMove(Position toPosition, Player player, Figure[][] board) {
-        //ToDo
-        return ErrorCode.Success;
+
+        if (isThereOwnFigureOn(toPosition, player, board)) {
+            return ErrorCode.TryAgain;
+        }
+
+        if (Math.abs(toPosition.getRow() - position.getRow()) == Math.abs(toPosition.getColumn() - position.getColumn())
+                && thePathIsClearDiagonally(toPosition, board)) {
+            return ErrorCode.Success;
+        }
+
+        return ErrorCode.TryAgain;
+    }
+
+    private boolean thePathIsClearDiagonally(Position toPosition, Figure[][] board) {
+
+        int fromRow;
+        int toRow;
+        int column;
+
+        if (position.getRow() < toPosition.getRow()) {
+            fromRow = position.getRow();
+            toRow = toPosition.getRow();
+            column = position.getColumn();
+        } else {
+            fromRow = toPosition.getRow();
+            toRow = position.getRow();
+            column = toPosition.getColumn();
+        }
+
+        if (toPosition.getRow() + toPosition.getColumn() == position.getRow() + position.getColumn()) {
+            return isTheOppositeDiagonalClear(fromRow, toRow, column, board);
+        }
+
+        return isTheDiagonalClear(fromRow, toRow, column, board);
+    }
+
+    private boolean isTheDiagonalClear(int fromRow, int toRow, int column, Figure[][] board) {
+        ++fromRow;
+        ++column;
+
+        for (int i = fromRow; i < toRow; ++i) {
+            if(board[i][column] != null) {
+                return false;
+            }
+            ++column;
+        }
+
+        return true;
+    }
+
+    private boolean isTheOppositeDiagonalClear(int fromRow, int toRow, int column, Figure[][] board) {
+        ++fromRow;
+        --column;
+
+        for (int i = fromRow; i < toRow; ++i) {
+            if(board[i][column] != null) {
+                return false;
+            }
+            --column;
+        }
+
+        return true;
     }
 }
