8d5a3ce5a9eb85cc4952db153a433ba8ecf92e3b
ToDoRV <tr.velikov@gmail.com>
2019-09-06 13:32:49 +0300

    Realized canMove method

--- src/figure/Rook.java
+++ src/figure/Rook.java
@@ -20,7 +20,51 @@ public class Rook extends Figure {
 
     @Override
     public ErrorCode canMove(Position toPosition, Player player, Figure[][] board) {
-        //ToDo
-        return ErrorCode.Success;
+
+        if (isThereOwnFigureOn(toPosition, player, board)) {
+            return ErrorCode.TryAgain;
+        }
+
+        if (position.getRow() == toPosition.getRow()
+                && thePathIsClearByColumn(toPosition, board)) {
+            return ErrorCode.Success;
+        }
+
+        if (position.getColumn() == toPosition.getColumn()
+                && thePathIsClearByRow(toPosition, board)) {
+            return ErrorCode.Success;
+        }
+
+        return ErrorCode.TryAgain;
+    }
+
+    private boolean thePathIsClearByColumn(Position toPosition, Figure[][] board) {
+        int row = position.getRow();
+        int fromColumn = Math.min(position.getColumn(), toPosition.getColumn());
+        int toColumn = Math.max(position.getColumn(), toPosition.getColumn());
+
+        ++fromColumn;
+        for (int i = fromColumn; i < toColumn; ++i) {
+            if (board[row][i] != null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean thePathIsClearByRow(Position toPosition, Figure[][] board) {
+        int column = position.getColumn();
+        int fromRow = Math.min(position.getRow(), toPosition.getRow());
+        int toRow = Math.max(position.getRow(), toPosition.getRow());
+
+        ++fromRow;
+        for (int i = fromRow; i < toRow; ++i) {
+            if (board[i][column] != null) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
